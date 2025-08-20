4f0854e5a370590edbee9aa4c29106e10d1d8739
Kaviarasan79 <kaviarasan0709@gmail.com>
2025-08-20 10:00:23 +0530

    commit

--- C2TC_COREJAVAASSIGNMENT/src/Assignment_2/Three.java
+++ C2TC_COREJAVAASSIGNMENT/src/Assignment_2/Three.java
@@ -1,6 +1,6 @@
 package Assignment_2;
-public class three {
-    public three() {
+public class Three {
+    public Three() {
     	// default ah super method call agum 
         System.out.println("Student object is created");
     }
